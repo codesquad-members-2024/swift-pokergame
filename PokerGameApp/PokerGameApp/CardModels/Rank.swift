64a2c8b4b9c582a336dd15e456edf7763b21c029
joho2022 <josama1997@naver.com>
2024-03-13 01:00:18 +0900

    feat: 카드덱 구현하고 테스트하기

--- PokerGameApp/PokerGameApp/CardModels/Rank.swift
+++ PokerGameApp/PokerGameApp/CardModels/Rank.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // 카드 숫자라는 관련된 값들을 그룹으로 정의하기 위해서 enum을 사용
-enum Rank: Int {
+enum Rank: Int, CaseIterable {
     case ace = 1, two, three, four, five, six, seven, eight, nine, ten
     case jack = 11
     case queen = 12
--- PokerGameApp/PokerGameApp/CardModels/Suit.swift
+++ PokerGameApp/PokerGameApp/CardModels/Suit.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // 카드 모양이라는 관련된 값들을 그룹으로 정의하기 위해서 enum을 사용
-enum Suit: String {
+enum Suit: String, CaseIterable {
     case hearts = "\u{2665}" // ♥️
     case diamonds = "\u{2666}" // ♦️
     case clubs = "\u{2663}" // ♣️
