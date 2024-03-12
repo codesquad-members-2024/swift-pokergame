9923c341311f0b6e9f1798dc17fb646b94068734
joho2022 <josama1997@naver.com>
2024-03-12 14:22:38 +0900

    feat: 카드 클래스 구현하기

---
+++ PokerGameApp/PokerGameApp/CardModels/Suit.swift
@@ -0,0 +1,16 @@
+//
+//  Suit.swift
+//  PokerGameApp
+//
+//  Created by 조호근 on 3/12/24.
+//
+
+import Foundation
+
+// 카드 모양이라는 관련된 값들을 그룹으로 정의하기 위해서 enum을 사용
+enum Suit: String {
+    case hearts = "\u{2665}" // ♥️
+    case diamonds = "\u{2666}" // ♦️
+    case clubs = "\u{2663}" // ♣️
+    case spades = "\u{2660}" // ♠️
+}
