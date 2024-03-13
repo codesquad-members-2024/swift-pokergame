0caa80527bf0cd2f5fe2909ad02dd3a3d181353f
joho2022 <josama1997@naver.com>
2024-03-13 14:23:25 +0900

    refactor: Equatable 및 CustomStringConvertible 채택

--- PokerGameApp/PokerGameApp/CardModels/PokerCard.swift
+++ PokerGameApp/PokerGameApp/CardModels/PokerCard.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // 명확하게 이해하기 위해서 모양과 숫자를 독립적인 구성으로 나눴다.
-class PokerCard {
+class PokerCard: Equatable, CustomStringConvertible {
     var suit: Suit
     var rank: Rank
     
@@ -17,8 +17,13 @@ class PokerCard {
         self.rank = rank
     }
     
-    func describe() -> String {
-        return "\(suit.rawValue)\(rank.description)"
+    var description: String {
+        return "\(suit)\(rank)"
     }
+    
+    static func == (lhs: PokerCard, rhs: PokerCard) -> Bool {
+        return lhs.suit == rhs.suit && lhs.rank == rhs.rank
+    }
+    
 }
- 
+
