7fb2aa4b6819d920a6086cc6d69e2a85a08b0f5a
joho2022 <josama1997@naver.com>
2024-03-15 11:00:25 +0900

    feat: 포커게임 화면만들기

--- PokerGameApp/PokerGameApp/CardBoardViewController.swift
+++ PokerGameApp/PokerGameApp/CardBoardViewController.swift
@@ -8,6 +8,21 @@
 import UIKit
 
 class CardBoardViewController: UIViewController {
+    private var segmentStackView: UIStackView!
+    var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stackView
+    }()
+    
+    private var selectedGameType: GameType = .sevenCard
+    private var selectedPlayerCount: PlayerType = .two
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,54 +32,160 @@ class CardBoardViewController: UIViewController {
             view.backgroundColor = .brown
         }
         
-        setupView()
-        
-        var pokerGame = PokerGame(gameType: .sevenCard, playerCount: 3)
-        pokerGame.startGame()
-        pokerGame.printPlayerAndDealerCards()
+        setupSegmentControl()
+        setupMainStackView()
+        updateGame()
         
     }
-}
-
-private extension CardBoardViewController {
-    func setupView() {
-        
-        let totalCards = 7
-        let cardAspectRatio: CGFloat = 1.27
-        let stackViewSpacing: CGFloat = 10.0
-        
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.alignment = .fill
-        stackView.spacing = stackViewSpacing
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        for _ in 0..<totalCards {
-            let cardImageView = UIImageView()
-            
-            if let backOfCardImage = UIImage(named: "card-back") {
-                cardImageView.image = backOfCardImage
-            } else {
-                cardImageView.image = UIImage(systemName: "menucard.fill")!.withTintColor(.white, renderingMode: .alwaysOriginal)
-            }
-            
-            cardImageView.contentMode = .scaleAspectFit
-            cardImageView.clipsToBounds = true
-            stackView.addArrangedSubview(cardImageView)
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            updateGame()
         }
-        
-        view.addSubview(stackView)
-        
+    }
+    
+    
+    private func setupMainStackView() {
+        view.addSubview(mainStackView)
+        setupMainStackViewConstraints()
+    }
+
+    private func setupMainStackViewConstraints() {
         NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            stackView.heightAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: cardAspectRatio / CGFloat(totalCards)) // 스택 뷰의 높이를 그 너비의 약 1/7에 해당하는 1.27배로 설정해야 함
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            mainStackView.topAnchor.constraint(equalTo: segmentStackView.bottomAnchor, constant: 20),
         ])
     }
 }
 
+private extension CardBoardViewController {
+    func setupCardsAndNameStack(with name: String, and cards: [String]) {
+        
+        let verticalStackViewSpacing: CGFloat = 1.0
+        let cardStackViewSpacing: CGFloat = 1.0
+        
+        let totalCards = cards.count
+        let cardAspectRatio: CGFloat = 1.27
+        let availableWidth = view.bounds.width - 40
+        let cardWidth = (availableWidth - (cardStackViewSpacing * CGFloat(totalCards - 1))) / CGFloat(totalCards)
+        let cardHeight = cardWidth * cardAspectRatio
+        
+        let verticalStackView = UIStackView()
+        verticalStackView.axis = .vertical
+        verticalStackView.distribution = .fill
+        verticalStackView.alignment = .fill
+        verticalStackView.spacing = verticalStackViewSpacing
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let nameLabel = UILabel()
+        nameLabel.text = name
+        nameLabel.textAlignment = .left
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        let cardStackView = UIStackView()
+        cardStackView.axis = .horizontal
+        cardStackView.distribution = .fillEqually
+        cardStackView.alignment = .fill
+        cardStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        cards.forEach { description in
+            let cardImageView = UIImageView()
+            cardImageView.contentMode = .scaleAspectFit
+            cardImageView.clipsToBounds = true
+            let frontImageName = description
+            
+            if let frontImage = UIImage(named: frontImageName) {
+                cardImageView.image = frontImage
+            } else {
+                cardImageView.image = UIImage(named: "card-back")
+            }
+            cardImageView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                cardImageView.widthAnchor.constraint(equalToConstant: cardWidth),
+                cardImageView.heightAnchor.constraint(equalToConstant: cardHeight)
+            ])
+            cardStackView.addArrangedSubview(cardImageView)
+        }
+        
+        
+        verticalStackView.addArrangedSubview(nameLabel)
+        verticalStackView.addArrangedSubview(cardStackView)
+        
+        mainStackView.addArrangedSubview(verticalStackView)
+    }
+    
+    func setupSegmentControl() {
+        let gameTypeSegmentedControl = UISegmentedControl(items: ["7 Cards", "5 Cards"])
+        let playerCountSegmentedControl = UISegmentedControl(items: ["2명", "3명", "4명"])
+        
+        gameTypeSegmentedControl.selectedSegmentIndex = 0
+        playerCountSegmentedControl.selectedSegmentIndex = 0
+        
+        gameTypeSegmentedControl.addTarget(self, action: #selector(gameTypeChanged(_:)), for: .valueChanged)
+        playerCountSegmentedControl.addTarget(self, action: #selector(playerCountChanged(_:)), for: .valueChanged)
+        
+        segmentStackView = UIStackView(arrangedSubviews: [gameTypeSegmentedControl, playerCountSegmentedControl])
+        segmentStackView.axis = .vertical
+        segmentStackView.distribution = .fillEqually
+        segmentStackView.spacing = 10
+        segmentStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(segmentStackView)
+        
+        NSLayoutConstraint.activate([
+            segmentStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            segmentStackView.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            segmentStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            segmentStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100)
+        ])
+    }
+    
+    @objc func gameTypeChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            selectedGameType = .sevenCard
+        case 1:
+            selectedGameType = .fiveCard
+        default:
+            break
+        }
+        updateGame()
+    }
+    
+    @objc func playerCountChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            selectedPlayerCount = .two
+        case 1:
+            selectedPlayerCount = .three
+        case 2:
+            selectedPlayerCount = .four
+        default:
+            break
+        }
+        updateGame()
+    }
+    
+    private func updateGame() {
+        let arrangedSubviews = mainStackView.arrangedSubviews
+        for subview in arrangedSubviews {
+            mainStackView.removeArrangedSubview(subview)
+            subview.removeFromSuperview()
+        }
+        
+        let game = PokerGame(gameType: selectedGameType, playerCount: selectedPlayerCount)
+        
+        game.startGame()
+        
+        let (names, cardsArray) = game.getPlayersInfo()
 
-
-
+        for (name, cards) in zip(names, cardsArray) {
+            setupCardsAndNameStack(with: name, and: cards)
+        }
+        
+        let (dealerName, dealerCards) = game.getDealerInfo()
+        setupCardsAndNameStack(with: dealerName, and: dealerCards)
+    }
+}
